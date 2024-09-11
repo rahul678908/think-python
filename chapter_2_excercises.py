40740b4a9d0e7188d6fa92bdde3c574a5b400e70
rahul678908 <rahulkannan33886@gmail.com>
2024-09-11 22:36:12 +0530

    Add files via upload

---
+++ chapter_2_excercises.py
@@ -0,0 +1,25 @@
+x = y = 1
+z = 2
+print(x-y+z)
+
+x = 2
+y = 4
+print(x*y)
+
+volumeofsphere = 4/3*3.14*x*3*3*3
+x = 3
+result = volumeofsphere*x
+print(result)
+
+bookprice = 24.95
+discount = (40/100)
+point = 0.4
+v = bookprice*discount
+print(bookprice*discount)
+shippingcost = 3 
+print(v+shippingcost)
+additionalcopies = 75
+wholesalecost = 60
+print(v+shippingcost*additionalcopies*wholesalecost)
+
+
