ed9ad844d3839f4b3eb5bf7719d481d108e80f22
rahul678908 <rahulkannan33886@gmail.com>
2024-09-11 12:06:02 +0530

    Add files via upload

---
+++ 2.1 assignment statements.py
@@ -0,0 +1,14 @@
+message = 'and now for something completely different'
+n = 17
+pi = 3.1415926535897932
+print(message)
+print(17)
+print(pi)
+
+f = 42 
+n = 17
+fn = n + 25 
+print(fn)
+
+miles = 26.2
+print(miles * 1.61)
