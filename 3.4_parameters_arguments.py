e87d49743e2fce917bdcfecd90ae9251f9418312
rahul678908 <rahulkannan33886@gmail.com>
2024-09-12 12:46:35 +0530

    Add files via upload

---
+++ 3.4_parameters_arguments.py
@@ -0,0 +1,19 @@
+def print_lyrics():
+    print("I'm a lumerjack looking for a cow")
+    print("I sleep all night and work all day")
+
+print(print_lyrics)
+
+def repeat_lyrics():
+    print_lyrics()
+    print_lyrics()
+
+a = repeat_lyrics()
+
+
+    # print(repeat_lyrics)
+    # (print_lyrics)
+    # <class 'function'>
+
+# (print_lyrics)
+     
