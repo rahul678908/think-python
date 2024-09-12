7bde10da7f1f5a98f34a772f2f7ab9417b98a0c5
rahul678908 <rahulkannan33886@gmail.com>
2024-09-12 09:48:41 +0530

    Add files via upload

---
+++ 3.1_Functions.py
@@ -0,0 +1,18 @@
+a = int('32')
+print(a)
+
+b = int(3.99999)
+print(b)
+
+c = int(-2.3)
+print(c)
+
+d = float(32)
+print(d)
+e = float('2.98984')
+print(e)
+
+f = str(32)
+print(f)
+g = str(5.25678)
+print(g)
