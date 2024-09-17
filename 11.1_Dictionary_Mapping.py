bd0520e904a2c094cea12e4ab0ae8605cdcb42ba
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 10:16:39 +0530

    Add files via upload

---
+++ 11.1_Dictionary_Mapping.py
@@ -0,0 +1,17 @@
+eng2sp = dict()
+print(eng2sp)
+
+eng2sp['one'] = 'uno'
+print(eng2sp)
+
+eng2sp = {'one' : 'uno', 'two':'dos','three' : 'tres'}
+print(eng2sp)
+
+print(len(eng2sp))
+
+print('one' in eng2sp)
+
+print('uno' in eng2sp)
+
+vals = eng2sp.values()
+print('uno' in vals)
