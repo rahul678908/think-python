6af74a84a46313d708696920659c8446ee199776
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 13:19:36 +0530

    Add files via upload

---
+++ if-elif-else-divisible.py
@@ -0,0 +1,27 @@
+n = 8
+if n % 2 == 0:
+    print("number is divisible by 2")
+elif n % 3 == 0:
+    print("number is divisible by 3")
+else:
+    print("number is neither divisible by 2 or 3")
+print("Done!")
+
+n = 15
+if n % 2 == 0:
+    print("number is divisible by 2")
+elif n % 3 == 0:
+    print("number is divisible by 3")
+else:
+    print("number is neither divisible by 2 or 3")
+print("Done!")
+
+n = 17
+if n % 2 == 0:
+    print("number is divisible by 2")
+elif n % 3 == 0:
+    print("number is divisible by 3")
+else:
+    print("number is neither divisible by 2 or 3")
+print("Done!")
+
