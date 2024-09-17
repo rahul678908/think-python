2477f898aafc952acd117cc3bee19b0759648028
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 12:00:12 +0530

    Add files via upload

---
+++ if_statement.py
@@ -0,0 +1,23 @@
+# introduction of if
+x = 10
+if x < 50:
+    print("x is less than 50")
+
+# outside if
+x = 60
+if x < 50:
+    print("y is less than 50")
+    print("End of the program")
+
+# shorthand if
+z = 10
+if z <50: print("x is less than 50")
+print("end of the program")
+
+#nested if
+l = 10
+if l < 50:
+    if l == 10:
+        print("l is less than 50")
+        print("l is equal to 10")
+        print("End of the program")
