a9df9c26519d886cc70624986e26f0c9ee761484
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 12:47:28 +0530

    Add files via upload

---
+++ if_else_statement.py
@@ -0,0 +1,14 @@
+age = 20
+if age < 18:
+    print("you can't vote.")
+else:
+    print("you can vote.")
+    print("Done")
+
+age = 16
+if age < 18:
+    print("you can't vote.")
+else:
+    print("you can vote")
+    print("Done.")
+
