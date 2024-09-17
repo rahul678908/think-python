ecbc005f2c3ecd7a17b125a763609a80edfb9e7e
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 13:12:12 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,26 @@
+x = 5
+if x == 5:
+    print("x is 5")
+elif x < 5:
+    print("x is less than 5")
+else:
+    print("x is greater than 5")
+print("Done!")
+
+x = -2
+if x == 5:
+    print("x is 5")
+elif x < 5:
+    print("x is less than 5")
+else:
+    print("x is greater than 5")
+print("Done!")
+
+x = 8
+if x == 5:
+    print("x is 5")
+elif x < 5:
+    print("x is less than 5")
+else:
+    print("x is greater than 5")
+print("Done!")
