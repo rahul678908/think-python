20c0ed783a5b8b402dc1b4e0fe93907f1bf79dd2
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 16:55:07 +0530

    Add files via upload

---
+++ for_loop_range_1.py
@@ -0,0 +1,15 @@
+for i in range(5):
+    print(i)
+print("Done!")
+
+for j in range(1, 6):
+    print(j)
+print("Done!")
+
+for p in range(-1, 8):
+    print(p)
+print("Done!")
+
+for k in range(1, 10, 2):
+    print(k)
+print("Done!")
