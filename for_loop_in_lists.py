c7730fadbbc16017db2859a8a25bb727b76894b7
rahul678908 <rahulkannan33886@gmail.com>
2024-09-17 14:20:03 +0530

    Add files via upload

---
+++ for_loop_in_lists.py
@@ -0,0 +1,24 @@
+cars = ['Audi', 'BMW', 'Toyota']
+for car in cars:
+    print(car)
+
+fruits = ['Apple', 'Orange', 'grapes']
+for vegetable in fruits:
+    print(vegetable)
+
+# Iterating over a list using for loop and range
+
+abc = ['Lenovo','HP','Dell']
+for i in range(len(abc)):
+    print(abc[i])
+
+# list comprehension
+
+bike = ['splendor','honda','yamaha']
+[print(bikes)for bikes in bike]
+
+movie = ['titanic','avatar','antman']
+[print(movies)for movies in movie]
+
+phone = ['iqoo','samsung','apple']
+[print(phones)for phones in phone]
